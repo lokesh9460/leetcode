b5906b5d9ade545414996bbe6f0418e40aa58bb7
Lokesh Kumar <39955241+lokesh9460@users.noreply.github.com>
2021-01-18 22:32:59 +0530

    0 ms

--- solutions/1672. Richest Customer Wealth.cpp
+++ solutions/1672. Richest Customer Wealth.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
+        int cols=accounts[0].size();
+        int rows=accounts.size();
+        vector<int> arr;
+        for(int i=0;i<rows;i++)
+        {
+            int sum=0;
+            for(int j=0;j<cols;j++)
+                sum+=accounts[i][j];
+            arr.push_back(sum);
+        }
+        return  *max_element(arr.begin(),arr.end());
         
     }
 };

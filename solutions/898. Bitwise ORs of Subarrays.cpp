1dc0c9419c0f6bce776a8e2ca2c32951a3f2d174
Lokesh Kumar <39955241+lokesh9460@users.noreply.github.com>
2021-01-20 17:11:35 +0530

    440 ms

---
+++ solutions/898. Bitwise ORs of Subarrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        unordered_set<int> s;
+        for(int i=0;i<arr.size();i++)
+        {
+            int j=i-1;
+            int x=0,y=arr[i];
+            s.insert(y);
+            while(j>=0 and x!=y)
+            {
+                x|=arr[j];
+                y|=arr[j];
+                s.insert(y);
+                j--;
+            }
+        }
+        return s.size();
+        
+        
+    }
+};

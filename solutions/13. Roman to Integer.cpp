3846b7426a01d1b401bd86dce7c291c263556c7c
Lokesh Kumar <39955241+lokesh9460@users.noreply.github.com>
2021-01-30 00:39:08 +0530

    12 ms

---
+++ solutions/13. Roman to Integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int ans=mp[s[0]];
+        for(int i=1;i<s.size();i++)
+        {
+           if(mp[s[i]]>mp[s[i-1]])
+           {
+               ans-=mp[s[i-1]];
+               ans+=(mp[s[i]]-mp[s[i-1]]);
+           }
+            else
+            {
+                ans+=mp[s[i]];
+            }
+        }
+        return ans;
+        
+    }
+};

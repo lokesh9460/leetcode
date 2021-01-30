8c4737128e58e5d45557d6da7f199311b234b06d
Lokesh Kumar <39955241+lokesh9460@users.noreply.github.com>
2021-01-30 13:53:11 +0530

    0 ms

---
+++ solutions/128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>s;
+        for(auto x:nums)
+            s.insert(x);
+        int res=0;
+        for(auto x:nums)
+        {
+            if(s.count(x) && !s.count(x-1))
+            {
+                int y=x;
+                s.erase(x);
+                while(s.count(y+1))
+                {
+                    y++;
+                    s.erase(y);
+                }
+                res=max(res,y-x+1);
+            }
+        }
+        return res;
+        
+    }
+};
